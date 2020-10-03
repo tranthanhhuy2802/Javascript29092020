f725237bbe05d01c30d901f0706c453ff9ad05cf
Huy <thanhhuy2802@gmail.com>
2020-10-03 18:19:07 +0700

    buoi2.js

---
+++ buoi1.js
@@ -0,0 +1,25 @@
+// khai bao bien
+// mutable: var (thay doi duoc)
+// immutable: const (khong thay doi dc)
+// var a = 10;
+// const b = 10;
+
+// a = "10";
+// b = "11";
+// console.log(a);
+// console.log(b);
+
+// 2 kieu du lieu
+// null
+// undefined
+// TH1: khai bao 1 bien ma ko cap gia tri (chi ap dung var, let)
+// var a
+// console.log(a)
+// TH2 : truy van toi thuoc tinh khong ton tai cua object
+//Json
+//3 : object(tim hieu camelcase)
+// const teo = {
+//     name: "Nguyen Van Teo",
+//     age : 20
+// }
+// console.log(teo.name.a)
---
+++ buoi2.js
@@ -0,0 +1,8 @@
+//4 Array
+const arrNames = ["Teo", "ti", "tun"];
+// console.log(arrNames[0]); //cách truy cập thông thường
+
+// const teo = {
+//   name: "Nguyen van TEo",
+// };
+// console.log(teo["name"]);
